dd4c8f53ec01304e43d562de0da7a9ad0ae72d33
Jérémy Halin <admin@jeremyhalin.fr>
2015-05-12 00:45:38 +0200

    nosniff et async javascript

--- index.php
+++ index.php
@@ -152,8 +152,8 @@
 </section>
 
 <?php include('footer.php'); ?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" async=></script>
-<script src="js/jquery.onscreen.min.js" async=></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" async></script>
+<script src="js/jquery.onscreen.min.js" async></script>
 <script src="js/main.js" async></script>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
